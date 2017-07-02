8ab52125768a319939054c7811f340ccd0c82c39
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-02 14:48:41 -0400

    Rendering with Component, Render and Children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-const App = () => <h1>Hello World!</h1>
+const Home = ({match}) => {
+    console.log(match)
+    return <h1>Home</h1>
+}
+const AboutComp = () => <h2>This is the About component</h2>
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+      {/*<Route path='/about' component={AboutComp} />*/}
+      {/*<Route path='/about' render={ () => (<h2>New About</h2>) }/>*/}
+      <Route path='/about' children={({match}) => match && <h2>I will always render</h2>} />
+    </div>
+  </Router>
+)
 
 export default App
