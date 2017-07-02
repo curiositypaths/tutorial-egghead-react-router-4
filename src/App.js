5e11334e9cf5c83919d1b8ba1db645951d3e45db
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-02 17:25:22 -0400

    URL params

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css'
 
 const isActiveFn = (match, location) => {
@@ -8,21 +8,15 @@ const isActiveFn = (match, location) => {
   return match
 }
 
-const Links = () => (
-  <nav>
-    <NavLink exact activeClassName='active-link' to='/'>Home</NavLink>
-    <NavLink activeClassName='active-link' to={ {pathname:'/about'} }>About</NavLink>
-    <NavLink isActive={ isActiveFn } activeClassName='active-link' replace to='/Contact'>Contact</NavLink>
-  </nav>
-)
-
 const App = () => (
   <Router>
     <div>
-      <Links/>
-      <Route exact path='/' render={ ()=> <h1>Home</h1> } />
-      <Route exact path='/about' render={ ()=> <h1>About</h1> } />
-      <Route exact path='/contact' render={ ()=> <h1>Contact</h1> } />
+      <Route path='/:page?-:subpage?' render={ ({match}) => (
+        <h1>
+          Page: {match.params.page || 'Home'}<br/>
+          Subpage: {match.params.subpage}
+        </h1>
+      ) } />
     </div>
   </Router>
 )
