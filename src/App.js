d55cd1a37b8c15b7f8268dae9ecce4781661f13f
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-02 20:01:35 -0400

    Redirect with params

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import './App.css'
 const Links = () =>
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/old">Old</Link>
-    <Link to="/new">New</Link>
+    <Link to="/old/456">Old</Link>
+    <Link to="/new/123">New</Link>
   </nav>
 
     const App = (props) => (
@@ -19,8 +19,8 @@ const Links = () =>
     <div>
       <Links />
       <Route exact path="/" render={ () => <h1>Home</h1> } />
-      <Route exact path="/new" render={ () => <h1>New</h1> } />
-      <Route path="/old" render={ () => <Redirect to="/new" /> } />
+      <Route exact path="/new/:str" render={ ({match}) => (<h1>New: {match.params.str}</h1>) } />
+      <Route path="/old/:str" render={ ({match}) => (<Redirect to={`/new/${match.params.str}`} />) } />
     </div>
   </Router>
 )
