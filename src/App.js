4c3697a9439bd34af3b18b4e2576947e0955c1b7
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-02 20:09:48 -0400

    Conditional routing

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import {BrowserRouter as Router,
 } from 'react-router-dom'
 import './App.css'
 
+const loggedin = true
+
 const Links = () =>
   <nav>
     <Link to="/">Home</Link>
     <Link to="/old/456">Old</Link>
     <Link to="/new/123">New</Link>
+    <Link to="/protected">Protected</Link>
   </nav>
 
     const App = (props) => (
@@ -20,7 +23,11 @@ const Links = () =>
       <Links />
       <Route exact path="/" render={ () => <h1>Home</h1> } />
       <Route exact path="/new/:str" render={ ({match}) => (<h1>New: {match.params.str}</h1>) } />
-      <Route path="/old/:str" render={ ({match}) => (<Redirect to={`/new/${match.params.str}`} />) } />
+      <Route path="/protected" render={ () => (
+        loggedin
+        ? <h1>Welcome to the protected page</h1>
+        : <Redirect to="/new/Login" />
+      ) } />
     </div>
   </Router>
 )
